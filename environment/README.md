e4a4b83c0321e697c67c7010ac7f142fe53dd379
Kemal Inecik <benkemalim@gmail.com>
2024-03-25 22:08:48 +0100

    Mamba Environments

--- environment/README.md
+++ environment/README.md
@@ -24,11 +24,28 @@ Follow the steps.
 3. Install the environment.
 
    > Note: [mamba](https://github.com/conda-forge/miniforge) (mambaforge) improves install times drastically.
-   > All mamba commands can be replaced by `conda`.
-
-   ```bash
-   mamba env create -f environment/tardis_env.yaml  # For M1 Mac. No GPU support.
-   ```
+   > All mamba commands can be replaced by `conda`, not recommended though.
+    
+   - Hava a look at the guide: https://bioinformatics_core.ascgitlab.helmholtz-muenchen.de/it_hpc_documentation/Installations.html#MambaInstallation
+   - Use the following versions instead of the latest, 
+     - for Linux: https://github.com/conda-forge/miniforge/releases/download/23.11.0-0/Miniforge3-23.11.0-0-Linux-x86_64.sh
+     - for MAC Silicon: https://github.com/conda-forge/miniforge/releases/download/23.11.0-0/Miniforge3-23.11.0-0-MacOSX-arm64.sh
+   - After installation `.bashrc` put, 
+      ```bash
+      export PATH=$PATH:/home/icb/kemal.inecik/tools/apps/mamba/bin
+      ```
+   - Then run 
+      ```bash
+      conda init "$(basename "${SHELL}")"
+      ```
+   - For installing new environment
+      ```bash
+      mamba env create -f environments/fcvi_env.yaml  # For M1 Mac. No GPU support.
+      ```
+   - Build the jupter extensions.
+      ```bash
+      jupyter lab build
+      ```
 
    Currently, whether the GPU version of pytorch is installed depends on which node you install the environment on. That means that if you want to your code to reconnise GPUs when working on a cluster, please make sure you install the conda environments from a node that has access to a GPU. Test whether you installed GPU version using the following
    in Python console.
@@ -41,19 +58,14 @@ Follow the steps.
    In case you already have an environment installed that doesn't recognise the GPU on a GPU node, remove the environment name manually and then call above command line again.
 
    ```bash
-   mamba env remove -n cannot_env
+   mamba env remove -n tardis_env
    mamba clean -avvvy
+   # rm -rf .conda .jupyter .ipython .local .netrc .npm .nv .python_history .virtual_documents .yarn .config/matplotlib
    ```
 
-4. Create a branch branch `git checkout -b your_branch`
+4. Create a branch `git checkout -b your_branch`
 
-5. Build the jupter extensions.
-
-   ```bash
-   jupyter lab build
-   ```
-
-6. Open JupyterLab. Go to the Settings menu at the top. Select 'Settings Editor'.
+5. Open JupyterLab. Go to the Settings menu at the top. Select 'Settings Editor'.
    - 'Language Server Protocol' settings:
      - bla
    - 'Code Formatter' settings:
--- scripts/sync_drive.sh
+++ scripts/sync_drive.sh
@@ -1,27 +1,55 @@
-exit 1
+#!/bin/bash
 
-rclone sync -v --progress -L /lustre/groups/ml01/workspace/kemal.inecik/tardis_data gdrive:/tardis/tardis_data
+# Ensure the script stops on first error
+set -e
 
-rclone sync -v --progress /Users/kemalinecik/git_nosync/tardis/data gdrive:/tardis/tardis_data \
---exclude "/raw/**" \
---exclude "__pycache__/**" \
---exclude "*.DS_Store" \
---exclude ".idea/**" \
---exclude ".mypy_cache/**" \
---exclude "*.ipynb_checkpoints/**" \
---exclude ".git/**" 
+# Define paths
+SERVER_DATA_PATH="/lustre/groups/ml01/workspace/kemal.inecik/tardis_data"
+LOCAL_DATA_PATH="/Users/kemalinecik/git_nosync/tardis/data"
+DRIVE_DATA_PATH="gdrive:/tardis/tardis_data"
+REPO_PATH="/Users/kemalinecik/git_nosync/tardis"
+DRIVE_REPO_PATH="gdrive:/tardis/repo"
 
-rclone sync -v --progress /Users/kemalinecik/git_nosync/tardis gdrive:/tardis/repo \
---exclude "__pycache__/**" \
---exclude "*.DS_Store" \
---exclude ".idea/**" \
---exclude ".mypy_cache/**" \
---exclude "*.ipynb_checkpoints/**" \
---exclude ".git/**" \
---exclude "/data/**"
+# Define common exclude parameters as a string
+COMMON_EXCLUDES="--exclude '__pycache__/**' \
+                --exclude '*.DS_Store' \
+                --exclude '.idea/**' \
+                --exclude '.mypy_cache/**' \
+                --exclude '*.ipynb_checkpoints/**' \
+                --exclude '.git/**'"
 
-rclone sync -v --progress gdrive:/tardis/tardis_data /Users/kemalinecik/git_nosync/tardis/data --exclude "/raw/**"
-
-rclone sync -v --progress gdrive:/tardis/tardis_data /lustre/groups/ml01/workspace/kemal.inecik/tardis_data --exclude "/raw/**"
-
-
+# Loop indefinitely until the user chooses to exit
+while true; do
+    # Display the menu to the user
+    echo "Select an operation to perform:"
+    echo "1. Send Data from Server to Drive"
+    echo "2. Send Data from Local to Drive"
+    echo "3. Send Data from Drive to Local"
+    echo "4. Send Data from Drive to Server"
+    echo "5. Sync Repo from Local to Drive"
+    echo "q. Exit the script"
+    
+    # Read the user's choice
+    read -p "Enter your choice (1-5): " choice
+    
+    # Execute the corresponding command based on the user's choice
+    case $choice in
+        1) rclone sync -v --progress -L "$SERVER_DATA_PATH" "$DRIVE_DATA_PATH" $COMMON_EXCLUDES
+           ;;
+        2) rclone sync -v --progress "$LOCAL_DATA_PATH" "$DRIVE_DATA_PATH" $COMMON_EXCLUDES
+           ;;
+        3) rclone sync -v --progress "$DRIVE_DATA_PATH" "$LOCAL_DATA_PATH" $COMMON_EXCLUDES
+           ;;
+        4) rclone sync -v --progress "$DRIVE_DATA_PATH" "$SERVER_DATA_PATH" $COMMON_EXCLUDES
+           ;;
+        5) rclone sync -v --progress "$REPO_PATH" "$DRIVE_REPO_PATH" $COMMON_EXCLUDES \
+            --exclude '/data/**'
+           ;;
+        q) echo "Exiting script."
+           exit 0
+           ;;
+        *) echo "Invalid choice. Please enter a number between 1 and 5."
+           ;;
+    esac
+    echo "" # Print a newline for better readability before the menu is shown again
+done
