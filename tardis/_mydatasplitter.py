8e1c1289236ee2ae719578ae76e2f09a2d66fff2
Kemal Inecik <benkemalim@gmail.com>
2024-04-03 12:02:37 +0200

    Mini datasets debugging

--- tardis/_mydatasplitter.py
+++ tardis/_mydatasplitter.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 from scvi.dataloaders import DataSplitter
+from scvi.dataloaders._ann_dataloader import AnnDataLoader
+from torch.utils.data import DataLoader
 
+class MyDataLoader(DataLoader):
+    pass
+
+class MyAnnDataLoader(AnnDataLoader):
+    pass
 
 class MyDataSplitter(DataSplitter):
-    pass
+    data_loader_cls = MyAnnDataLoader
