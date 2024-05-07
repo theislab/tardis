fa98a28ad2bf069543ed86519e3ab0f36d67a6ec
Kemal Inecik <benkemalim@gmail.com>
2024-05-07 20:52:51 +0200

    analysis & experiments

---
+++ training/server/runs/figure2/suo_invae/invae.py
@@ -0,0 +1,64 @@
+from inVAE import FinVAE, NFinVAE
+
+import warnings
+import os
+import sys
+import gc
+import warnings
+
+import anndata as ad
+import scanpy as sc
+import copy
+import torch
+from pathlib import Path
+import networkx as nx
+from sklearn.neighbors import kneighbors_graph
+import numpy as np
+import scanpy as sc
+import pandas as pd
+import numpy as np
+import scipy.stats
+
+adata_file_path = os.path.join("/home/icb/kemal.inecik/lustre_workspace/tardis_data/processed", "dataset_complete_Suo.h5ad")
+assert os.path.isfile(adata_file_path), f"File not already exist: `{adata_file_path}`"
+adata = ad.read_h5ad(adata_file_path)
+adata.X = adata.X.astype(np.float32)
+print(adata_file_path, flush=True)
+print(adata, flush=True)
+
+def create_random_mask(shape, ratio_true, seed=None):
+    rng = np.random.default_rng(seed)
+    random_floats = rng.random(shape)
+    mask = random_floats < ratio_true
+    return mask
+
+print(f"CUDA: {torch.cuda.is_available()}", flush=True)
+
+adata_train_bool = create_random_mask(adata.shape[0], ratio_true=0.8, seed=0)
+adata_train = adata[adata_train_bool].copy()
+adata_valid = adata[~adata_train_bool].copy()
+
+inv_covar_keys = {
+    'cont': [],
+    'cat': []
+}
+
+spur_covar_keys = {
+    'cont': [],
+    'cat': ['organ', 'integration_donor', 'integration_library_platform_coarse'] #set to the keys in the adata
+}
+
+model = FinVAE(
+    adata = adata_train,
+    layer = None,
+    inv_covar_keys = inv_covar_keys,
+    spur_covar_keys = spur_covar_keys,
+    device="cuda"
+)
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    model.train(n_epochs=50, lr_train=0.001, weight_decay=0.0001, 
+                log_freq=1, print_every_n_epochs=1, log_dir=os.path.join(os.getcwd(), "invae.log"))
+
+model.save("/home/icb/kemal.inecik/lustre_workspace/tardis_data/models/invae.pt")
---
+++ training/server/runs/figure2/suo_invae/suo_scanvi.py
@@ -0,0 +1,87 @@
+import warnings
+import os
+import sys
+import gc
+import warnings
+import anndata as ad
+import scanpy as sc
+import copy
+import torch
+from pathlib import Path
+import networkx as nx
+from sklearn.neighbors import kneighbors_graph
+import numpy as np
+import scanpy as sc
+import pandas as pd
+import numpy as np
+import scvi
+import scipy.stats
+
+sys.path.append("/home/icb/kemal.inecik/work/codes/tardis")
+import tardis
+tardis.config = tardis.config_server
+print(f"CUDA used: {torch.cuda.is_available()}")
+
+adata_file_path = os.path.join(tardis.config.io_directories["processed"], "dataset_complete_Suo.h5ad")
+assert os.path.isfile(adata_file_path), f"File not already exist: `{adata_file_path}`"
+adata = ad.read_h5ad(adata_file_path)
+adata.obs["age"] = adata.obs["age"].astype("str").astype("category")
+
+n_epochs_kl_warmup = 400
+model_params = dict(
+    n_hidden=512,
+    n_layers=3, 
+    n_latent=24, 
+    gene_likelihood = "nb",
+    use_batch_norm = "none",
+    use_layer_norm = "both",
+    dropout_rate = 0.1,
+)
+
+train_params = dict(
+    max_epochs=1000,
+    train_size=0.8,
+    batch_size=512,
+    check_val_every_n_epoch=10,
+    learning_rate_monitor=True,
+    # early stopping:
+    early_stopping=True,
+    early_stopping_patience=150,
+    early_stopping_monitor="elbo_train",
+    plan_kwargs = dict(
+        n_epochs_kl_warmup=n_epochs_kl_warmup,
+        lr=1e-3,
+        weight_decay=1e-6,
+        # optimizer="AdamW"
+        # lr-scheduler:
+        reduce_lr_on_plateau=True,
+        lr_patience=100,
+        lr_scheduler_metric="elbo_train",
+    ),
+    limit_train_batches=0.5, 
+    limit_val_batches=0.5,
+)
+
+dataset_params = dict(
+    layer=None, 
+    labels_key="cell_type",
+    unlabeled_category="NA",
+    batch_key="concatenated_integration_covariates",
+    categorical_covariate_keys=None,
+)
+
+
+scvi.model.SCANVI.setup_anndata(adata, **dataset_params)
+
+vae = scvi.model.SCANVI(adata, **model_params)
+vae.train(**train_params)
+
+dir_path = os.path.join(
+    tardis.config.io_directories["models"],
+    "suo_scanvi"
+)
+
+vae.save(
+    dir_path,
+    overwrite=True,
+)
